4606eef543d1758313fbf1f507828c3c1c1776ae
Mrnayman <79077253+Mrnayman@users.noreply.github.com>
2021-02-15 11:45:56 +0100

    Add files via upload

---
+++ EndorinoOS 3.1/EndorinoOs31.bat
@@ -0,0 +1,703 @@
+@echo off
+title Booting EndorinoOs 3.1
+goto booting
+
+
+:booting
+cls
+color 0f
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo Booting EndorinoOs
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo _
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+cls
+ping 1.1.1.1 -n 1 -w 705 > nul
+Echo 		Endorino (c) Operating System
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo Version 2021.01.19/1.0
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo memory taken 8,00 kb
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo opening system
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 	(C) system 3.1 [d] 
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo (C) System version - 2021.02.05/1.0
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 720 kB, 1,44 MB, 2,88 MB	360 kB, 1,2 MB
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		disc error finding 
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo system cant find any disc error
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		Ram need 5 mb
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo starting system
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+goto hello
+
+
+:hello
+cls
+color 3e
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		Endorino Inc (c)
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 			presents
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		EndorinoOs 3.1
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+cls
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		EndorinoOs 3.1
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		HELLO WORLD
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+goto Selectuser
+
+
+:Selectuser
+cls
+color 1f
+echo [1] User 1 (Administrator)
+echo [2] User 2
+set /p wybieram:={/..=}
+if %wybieram:%==2 goto welcome1
+if %wybieram:%==1 goto welcomeadmin
+
+
+:welcome1
+cls
+echo Welcome "User 2"
+echo loading desktop
+ping localhost -n 3 >nul
+ping localhost -n 3 >nul
+goto desktop
+
+
+:welcomeadmin
+cls
+echo enter pin 
+set /p "pass=>"
+cls
+echo Serching password
+ping localhost -n 3 >nul
+if %pass%==1111 goto loadingdesktop
+if not %pass%==1111 goto bad1
+
+
+:bad1
+echo Pin you enter is bad try again
+pause
+goto welcomeadmin
+
+
+:loadingdesktop
+cls
+echo Welcome "User 1"
+echo loading desktop
+ping localhost -n 3 >nul
+ping localhost -n 3 >nul
+goto desktop
+
+
+:desktop
+title EndorinoOS 3.1
+cls
+color 8f
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [ [1][menu] 						%time%		]
+ping 1.1.1.1 -n 1 -w 705 > nul	
+set /p wybieram:={/..=}
+if %wybieram:%==1 goto menu
+if %wybieram:%==EndorinoCOM goto EndorinoCOM1
+
+
+:EndorinoCOM1
+cls
+goto EndorinoCOM
+
+:menu
+cls
+color 8f
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [1] Apps
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [2] Settings
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [3] Power
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [4] Exit
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul	
+set /p wybieram:={/..=}
+if %wybieram:%==1 goto Apps
+if %wybieram:%==2 goto Settings
+if %wybieram:%==3 goto Power
+if %wybieram:%==4 goto desktop
+
+
+:Power
+cls
+color 8f
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [1] Turn off
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [2] Restart
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [3] Reboot
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [4] Exit
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul	
+set /p wybieram:={/..=}
+if %wybieram:%==1 goto exit
+if %wybieram:%==2 goto booting
+if %wybieram:%==3 goto booting
+if %wybieram:%==4 goto menu
+
+
+:exit
+cls
+color 3e
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		Endorino Inc (c)
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 			presents
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		EndorinoOs 3.1
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+cls
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		EndorinoOs 3.1
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		Good bye
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+cls
+color 0f
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo Booting EndorinoOs
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo _
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+cls
+ping 1.1.1.1 -n 1 -w 705 > nul
+Echo 		Endorino (c) Operating System
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo Version 2021.01.19/1.0
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo memory taken 8,00 kb
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo turning off system
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 	(C) system 3.1 [d] 
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo (C) System version - 2021.02.05/1.0
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 720 kB, 1,44 MB, 2,88 MB	360 kB, 1,2 MB
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		disc error finding 
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo system cant find any disc error
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 		Ram need 5 mb
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo turning off system
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo now you can turn off program
+pause>>nul
+exit
+
+
+:Apps
+cls
+color 8f
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [1] Internet
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [2] Notepad
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [3] Calculator
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [4] Setting
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [5] Exit App menu
+set /p wybieram:={/..=}
+if %wybieram:%==1 goto internet
+if %wybieram:%==2 goto notepad
+if %wybieram:%==3 goto calculator
+if %wybieram:%==4 goto settings
+if %wybieram:%==5 goto menu
+
+
+:calculator
+cls
+title Calculator
+color 8f
+cls
+echo  Calculator
+set /p fn= enter first number:
+set /p sn= enter second number:
+set /p math= ( -, +, * , /):
+set /a ans=%fn%%math%%sn%
+echo : %ans%
+pause>nul
+echo Press any key to exit
+pause>nul
+goto Apps
+
+
+:internet
+cls
+color b0
+echo 	Internet Explorer 1.0
+echo 	[1] youtube
+echo 	[2] Google
+echo 	[3] wikipedia
+echo 	[4] Facebook
+echo 	[5] Endorino support
+echo 	[6] -
+echo 	[7]  Exit
+set /p wybieram:={/..=}
+if %wybieram:%==1 goto youtube
+if %wybieram:%==2 goto google
+if %wybieram:%==3 goto wikipedia
+if %wybieram:%==4 goto facebook
+if %wybieram:%==5 goto discord 
+if %wybieram:%==6 goto 
+if %wybieram:%==7 goto goback
+
+
+:youtube
+start https://www.youtube.com/
+goto internet 
+
+
+:google 
+start https://www.google.pl/?safe=active&safe=active
+goto internet
+
+
+:wikipedia
+start https://pl.wikipedia.org/wiki/
+goto internet
+
+
+:Facebook
+start https://www.facebook.com/
+goto internet
+
+
+:goback
+color 87
+goto Apps
+
+:discord
+start https://discord.gg/ZCQbeBcPUR
+goto internet
+
+
+:notepad 
+cls
+echo [1] start 1
+echo [2] start 2
+echo [3] start 3
+echo [4} exit
+set /p wybieram:={1t ;2;3}
+if %wybieram:%==1 goto 1
+if %wybieram:%==2 goto 2
+if %wybieram:%==3 goto 3
+if %wybieram:%==4 goto Apps
+
+
+:1
+Start notepad.txt
+goto notepad
+
+
+:2
+Start notepad2.txt
+goto notepad
+
+
+:3
+Start notepad3.txt
+goto notepad
+
+
+:settings
+cls
+color 8f
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo.
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [1] -
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [2] System Info
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [3] 
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [4] Anty virus scan
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo [5] exit
+set /p wybieram:={1t ;2;3}
+if %wybieram:%==1 goto 
+if %wybieram:%==2 goto sysinfo
+if %wybieram:%==3 goto 
+if %wybieram:%==5 goto Apps
+if %wybieram:%==4 goto antyvirusscan
+
+
+
+:antyvirusscan
+cls
+color 0a
+echo scaning disk
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 0/10 disk sections scaned
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 1/10 disk sections scaned
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 2/10 disk sections scaned
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 5/10 disk sections scaned
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 6/10 disk sections scaned
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 8/10 disk sections scaned
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 9/10 disk sections scaned
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo 10/10 disk sections scaned
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+ping 1.1.1.1 -n 1 -w 705 > nul
+echo Anty-virus cant find any virus
+pause
+goto settings
+
+
+:bios
+title bios
+cls
+echo enter pin 
+set /p "pass=>"
+cls
+echo Serching password
+ping localhost -n 3 >nul
+if %pass%==1111 goto enteringbios
+if not %pass%==1111 goto bad
+
+
+:bad
+echo Pin you enter is bad try again
+pause
+goto bios
+
+
+:enteringbios
+cls
+color 1f
+echo Welcome User 1 !
+ping localhost -n 3 >nul
+cls
+echo [1] Bios info
+echo [2] Reboot
+echo [3] Security mode
+echo [4] exit bios
+set /p wybieram:={1;2;3;4}
+if %wybieram:%==1 goto biosinfo
+if %wybieram:%==2 goto booting
+if %wybieram:%==4 goto exitbios
+if %wybieram:%==3 goto security
+
+
+:exitbios
+cls
+echo select system
+echo [1] EndorinoOs 3.1
+echo [2] MS-Dos
+echo [3] go back to bios
+set /p wybieram:={1t ;2;3}
+if %wybieram:%==1 goto booting
+if %wybieram:%==2 goto MS-Dos
+if %wybieram:%==3 goto enteringbios
+
+
+
+:MS-Dos
+start cmd.exe
+goto enteringbios
+
+
+
+:biosinfo
+cls
+echo.
+echo Bios version : 1.0
+echo Bios Language : English
+pause
+goto enteringbios
+
+
+
+:sysinfo
+cls 
+echo system : EndorinoOS 3.1
+echo version : 1.0.0
+echo language : English
+pause
+goto settings
+
+
+:EndorinoCOM
+color 0a
+set /p wybieram:={.../}
+if %wybieram:%==/users goto users
+if %wybieram:%==/bios goto bios
+if %wybieram:%==/help goto help
+if %wybieram:%==/show-password goto showpass
+if %wybieram:%==/leave goto desktop
+
+
+
+:users 
+echo.
+echo users list : User 1, User 2
+pause
+goto EndorinoCOM
+
+
+:help
+echo.
+echo /users show users list
+echo /bios enter bios
+echo /leave leaving EndorinoCOM
+echo /show-password shows users passwords
+echo /help show this list
+pause
+goto EndorinoCOM
+
+
+:showpass
+echo.
+echo User 1 : "1111"
+echo User 2 : -------
+pause
+goto EndorinoCOM
